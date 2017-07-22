42cf66f3b1cf83be9feadf26572ac8cdf69fc6c0
Yuya Minami <yuya373@me.com>
2017-07-22 19:10:56 +0900

    disable onSubmit in Token

--- app/components/Token.js.jsx
+++ app/components/Token.js.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 
 type Props = {
   token: string,
-  onChange: (value: string) => void,
+  onChange: (value: string) => void
 };
 
 export default function Token({
   token,
   onChange,
 }: Props) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit} >
       <div className="form-group">
         <label htmlFor="token">Token</label>
         <input
